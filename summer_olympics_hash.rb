b5f3221113ebee1822546a29aedfddc45b5c031d
anna-rose <917566@isd624.org>
2018-07-27 14:08:38 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -11,6 +11,8 @@ def add_a_key_value_pair(cities)
  cities[:Atlanta] = "1996"
 end
 
+puts cities 
+
 def iterate_through_hash(cities)
   create_olympics_hash.each do |item, date|
     "The #{date} summer olympics took place in #{item}."
